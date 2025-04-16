b67334d13e6cefa16e6af8bbd04154f47895ae10
drakoshllk <lkaratel@list.ru>
2025-04-16 14:30:26 +0700

    twelfth

--- 2.3/2.3.3.py
+++ 2.3/2.3.3.py
@@ -15,12 +15,3 @@ class Calculation:
         return self.__calculation_line
     def set_calculation_line(self, calculation_line):
         self.__calculation_line = calculation_line
-
-
-str = Calculation("12345")
-print(str.get_calculation_line())
-str.delete_last_symbol()
-print(str.get_calculation_line())
-print(str.get_last_symbol())
-str.set_last_symbol_calculation_line("a")
-print(str.get_calculation_line())
